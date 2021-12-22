0ba975592d3359cbe82d2f12e9c8512fb654096d
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-12-21 23:39:56 -0500

    feat(layout): worked on layout and navbar

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db = SQLAlchemy(app=app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app=app)
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,18 @@
 from flask.templating import render_template
 from application import app
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title="Home")
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title="Login")
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title="Login")
