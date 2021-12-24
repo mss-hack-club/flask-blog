ac191b556be8bc79a2394b6c0008259f0dab3c24
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-12-24 12:00:23 -0500

    feat(form-templates): added create post template

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from flask.templating import render_template
 from application import app
 from flask_login import login_user, current_user, logout_user, login_required
-from application.forms import LoginForm, RegistrationForm
+from application.forms import LoginForm, PostForm, RegistrationForm
 
 
 @app.route('/')
@@ -20,3 +20,8 @@ def login():
 def register():
     form = RegistrationForm()
     return render_template('register.html', title="Register", form=form)
+
+@app.route('/post/new')
+def new_post():
+    form = PostForm()
+    return render_template('create_post.html', title="Create Post", form=form)
